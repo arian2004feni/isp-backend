4b3276bfd202a0a55e9d53fcbafc21cb4fcc0744
arian2004 <arian2004feni@gmail.com>
2026-01-23 23:15:37 +0600

    database setup and user model design

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
 const app = require("./app.js");
+const connectDB = require("../config/database.js");
+
+// Connect MongoDB FIRST
+connectDB();
 
 // Start server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
